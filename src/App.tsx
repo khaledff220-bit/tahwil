8e91836819516f5fc146da17fb5c45b32ebf6bda
اسمك_على_جيت_هاب <_>
2026-04-02 14:59:54 +0200

    Final Fix: Integrated ScrollToTop and UI enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import ScrollToTop from './components/common/ScrollToTop';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col font-cairo" dir="rtl">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
         <Outlet />
